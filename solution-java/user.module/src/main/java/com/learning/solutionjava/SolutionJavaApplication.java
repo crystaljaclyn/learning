e729568e241cc800802b95ba769ff934e6cd5444
Tuan Nguyen <arik.arslan.nguyen@gmail.com>
2022-08-25 18:32:51 +0700

    init

---
+++ solution-java/user.module/src/main/java/com/learning/solutionjava/SolutionJavaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.solutionjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionJavaApplication.class, args);
+	}
+
+}
